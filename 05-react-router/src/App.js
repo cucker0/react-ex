05aa5ad4bcb44ccd284c219136dc57ab349d02a8
hanxiao <hanxiao2100@qq.com>
2018-09-22 15:24:56 +0800

    NavLink

--- 05-react-router/src/App.js
+++ 05-react-router/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'
 import Home from './components/Home'
 import News from './components/News'
 import Others from './components/Others'
@@ -12,9 +12,14 @@ class App extends Component {
       <div className="App">
         <Router>
           <div className="rlink">
-            <Link to='/'>主页</Link>
-            <Link to='/news'>新闻</Link>
-            <Link to='/others'>其他</Link>
+            {/*<Link to='/'>主页</Link>*/}
+            {/*<Link to='/news'>新闻</Link>*/}
+            {/*<Link to='/others'>其他</Link>*/}
+            <div className="navbar">
+	            <NavLink activeClassName='active' exact to='/'>主页</NavLink>
+	            <NavLink activeClassName='active' to='/news'>新闻</NavLink>
+	            <NavLink activeClassName='active' to='/others'>其他</NavLink>
+            </div>
 
             <hr/>
 	          {/* exact  精确匹配  */}
