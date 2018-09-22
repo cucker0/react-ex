e4a4d518e119ac530d17bb32db3497ac02e9df05
hanxiao <hanxiao2100@qq.com>
2018-09-22 11:31:18 +0800

    03-shengmingzhouqi 调试生命周期步骤

--- 03-shengmingzhouqi/src/App.js
+++ 03-shengmingzhouqi/src/App.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+
+class Child extends Component{
+  componentWillUnmount(){
+    console.log("这是子组件componentWillUnmount")
+  }
+  render(){
+    return(
+      <div>
+        子组件
+      </div>
+    )
+  }
+}
+
 class App extends Component {
+  constructor(){
+    super();
+    //初始化state
+    this.state = {
+      flag: false
+    }
+	  this.toggle = this.toggle.bind(this);
+  }
+
+  componentWillMount(){
+    console.log("componentWillMount")
+  }
+
+  componentDidMount(){
+    console.log("componentDidMount")
+  }
+
+  shouldComponentUpdate(){
+    //控制是否显示
+    console.log("shouldComponentUpdate");
+    // return true;
+    return false;
+  }
+
+  componentWillUpdate(){
+    console.log("componentWillUpdate")
+  }
+
+	componentDidUpdate(){
+		console.log("componentDidUpdate")
+	}
+
+  toggle(){
+    this.setState({
+      flag: !this.state.flag
+    })
+  }
+
+
   render() {
+    console.log("render")
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        react
+        <button onClick={this.toggle}>开/关子组件</button>
+        {this.state.flag ? <Child/> : ""}
       </div>
     );
   }
